02ca8153e3d0f8b2a0c8e880def299e445003888
rohith2334 <rohithsai12341@gmail.com>
2024-01-27 20:59:24 +0530

    spring security implementation

--- springboot-backend/src/main/java/com/app/demo/DemoApplication.java
+++ springboot-backend/src/main/java/com/app/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.manve.demo;
+package com.app.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springboot-backend/src/test/java/com/app/demo/DemoApplicationTests.java
+++ springboot-backend/src/test/java/com/app/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.manve.demo;
+package com.app.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
